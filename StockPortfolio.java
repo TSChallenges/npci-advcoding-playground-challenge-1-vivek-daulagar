b94a833875b886e139db07040213d8dd4619721b
vivek-daulagar <72880370+vivek-daulagar@users.noreply.github.com>
2024-11-30 15:45:30 +0530

    Update StockPortfolio.java

--- StockPortfolio.java
+++ StockPortfolio.java
@@ -4,26 +4,41 @@ class StockPortfolio {
     private int quantity;
     private double buyingPrice;
     private double currentPrice;
+    private double highestPrice;
 
     // TODO: Initialize stock details
     public StockPortfolio(String symbol, int quantity, double buyingPrice) {
         // TODO
+        this.symbol =symbol;
+        this.quantity =quantity;
+        this.buyingPrice = buyingPrice;
+        this.currentPrice = currentPrice;
+        this.highestPrice = buyingPrice;
+        
     }
 
     // TODO: Update current market price
     public void updatePrice(double newPrice) {
         // TODO
+       if(newPrice >0){
+            currentPrice =newPrice;
+            if(highestPrice<newPrice){
+                highestPrice=newPrice;
+            }
+        }
+        
     }
 
     // TODO: Calculate total profit or loss
     public double calculateProfit() {
         // TODO
-        return 0.0;
+        return (currentPrice - buyingPrice)* quantity;
+        
     }
 
     // TODO: Calculate current total value of holdings
     public double getCurrentValue() {
         // TODO
-        return 0.0;
+        return (currentPrice * quantity);
     }
 }
